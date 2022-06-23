a31bc6897ed0275bc037b9d20305a1cda61f197f
Naufal Rizqullah <naufalrm312@gmail.com>
2022-06-23 19:38:38 +0700

    fix api delete2

--- app/services/productServices.js
+++ app/services/productServices.js
@@ -19,32 +19,26 @@ module.exports = {
     return productRepository.findProduct(id);
   },
 
-  // aman
   async findProductPicByIdProduct(id) {
     return productRepository.findProductPicByIdProduct(id);
   },
 
-  // aman
   async addProduct(requestBody) {
     return productRepository.addProduct(requestBody);
   },
 
-  // aman
   async addProductPic(requestBody) {
     return productRepository.addProductPic(requestBody);
   },
 
-  // aman
   async updateProduct(idProduct, requestBody) {
     return productRepository.updateProduct(idProduct, requestBody);
   },
 
-  // aman
   async deleteProduct(id) {
     return productRepository.deleteProduct(id);
   },
 
-  // aman
   async deleteProductPic(id) {
     return productRepository.deleteProductPic(id);
   },
